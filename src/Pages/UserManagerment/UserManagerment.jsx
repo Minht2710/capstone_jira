603e629b9f49778b52504ae69fb4ca1635e374d1
Minh <NguyenCongMinh0993@gmail.com>
2024-05-15 22:56:54 +0700

    update: user manger

---
+++ src/Pages/UserManagerment/UserManagerment.jsx
@@ -0,0 +1,324 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { getUserThunk } from '../../redux/slice/userSlice'
+import { getLocalStorage } from '../../utils/util'
+import { useDispatch, useSelector } from 'react-redux';
+import { Drawer, Modal, Table } from 'antd';
+import * as yup from 'yup'
+import { useState } from 'react';
+import { useFormik } from 'formik';
+import InputCustom from '../../Components/Input/InputCustom';
+import { quanLyTaiKhoanServ } from '../../services/quanLyTaiKhoan/quanLyTaiKhoanServ';
+import AddUser from '../../layout/AddUser/AddUser';
+
+
+
+
+const UserManagerment = () => {
+    let disPatch = useDispatch()
+    let { accessToken } = getLocalStorage("user")
+    let [isReload, setIsReload] = useState(false)
+    useEffect((params) => {
+        disPatch(getUserThunk(accessToken))
+    }, [isReload]
+    )
+    let [data,setData]=useState()
+    let dataTam = useSelector((state) => state.userSlice.listUser)
+    useEffect(() => {
+      setData(dataTam)
+    },[dataTam]
+    )
+    // let data = []
+
+    // arrUser.map((item, index) => {
+    //     let itemClone = { ...item }
+    //     itemClone.stt = index + 1
+    //     data.push(itemClone)
+    // }
+    // )
+
+    const columns = [
+        {
+            title: 'STT',
+            dataIndex: 'stt',
+            key: 'stt',
+            render: (text, record, index) => index + 1,
+            showSorterTooltip: {
+                target: 'full-header',
+            },
+
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email',
+            sorter: {
+                compare: (a, b) => a.email.localeCompare(b.email),
+                multiple: 3,
+            }
+
+        },
+        {
+            title: 'Project Name',
+            dataIndex: 'name',
+            key: 'name',
+            sorter: {
+                compare: (a, b) => a.name.localeCompare(b.name),
+                multiple: 2,
+            }
+
+        },
+
+        {
+            title: 'Phone',
+            dataIndex: 'phoneNumber',
+            key: 'phoneNumber',
+
+            sorter: {
+                sorter: (a, b) => a.phoneNumber - b.phoneNumber,
+                multiple: 1,
+            }
+
+        },
+
+        {
+            title: 'Action',
+            key: 'action',
+            // không có dataIndex nên record mặc định có thể lấy dữ liệu từ cột khác mà không cần thêm tham số text
+            render: (record) => (
+                <div>
+                    <button
+                        onClick={() => {
+                            handelEditProject(record.userId);
+                        }}
+                        className='bg-blue-600 hover:bg-blue-800 border rounded ml-4 py-1 px-2 text-white '
+                    >Sửa</button>
+                    <button
+                        onClick={() => handelDeleteUser(record.userId)}
+                        className='bg-red-500 hover:bg-red-800 border rounded ml-4 py-1 px-2 text-white '
+                    >Xóa</button>
+                </div>
+            )
+        },
+
+    ];
+
+    let onChange = (pagination, filters, sorter, extra) => {
+        console.log('params', pagination, filters, sorter, extra);
+    };
+
+    // edit user
+    let [userDetail, setUserDetail] = useState()
+    let handelEditProject = async (userId) => {
+        quanLyTaiKhoanServ.getUserById(userId, accessToken)
+            .then((res) => {
+                console.log(res.data.content[0]);
+                setUserDetail(res.data.content[0])
+
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+
+
+
+        showDrawer()
+    }
+    useEffect(() => {
+        userDetail && setFieldValue("id", userDetail.userId);
+        userDetail && setFieldValue("email", userDetail.email);
+        userDetail && setFieldValue("name", userDetail.name);
+        userDetail && setFieldValue("phoneNumber", userDetail.phoneNumber);
+
+
+
+    }, [userDetail]);
+
+    // Drawer ant
+    const [open, setOpen] = useState(false);
+    const showDrawer = () => {
+        setOpen(true);
+    };
+    const onCloseDrawer = () => {
+        setOpen(false);
+    };
+
+
+    let { handleChange, handleSubmit, values, errors, handleBlur, touched, setFieldValue } = useFormik({
+        initialValues: {
+            id: "",
+            email: "",
+            name: "",
+            phoneNumber: "",
+
+        },
+        onSubmit: async (values) => {
+            quanLyTaiKhoanServ.updateUser(values)
+                .then((res) => {
+                    console.log(res.data.content);
+                    setIsReload(values)
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+
+
+
+        },
+        validationSchema: yup.object({
+            id: yup.string().required("Vui lòng không bỏ trống")
+            , email: yup.string().email("Vui lòng kiểm tra định dạng email").required("Vui lòng không bỏ trống")
+            , name: yup.string().required("Vui lòng không bỏ trống")
+            , phoneNumber: yup.string().matches(/^(0[2|3|4|5|6|7|8|9]{1})([0-9]{8,9})$/, "Vui lòng nhập đúng số điện thoại").required("Vui lòng không bỏ trống")
+
+        })
+    })
+
+    let handelDeleteUser = (userId) => {
+        quanLyTaiKhoanServ.deleteUser(userId, accessToken)
+            .then((res) => {
+                console.log(res.data.content);
+                setIsReload(userId)
+            })
+            .catch((err) => {
+                console.log(err.response.data.content);
+            });
+
+    }
+
+    // searchKeyword
+    const [searchKeyword, setSearchKeyword] = useState("");
+
+    const handleSearchChange = (e) => {
+        quanLyTaiKhoanServ.getUserById(e.target.value, accessToken)
+            .then((res) => {
+                console.log(res.data.content);
+                setData(res.data.content)
+                
+
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+
+        setSearchKeyword(e.target.value);
+    };
+
+    const [visible, setVisible] = useState(false);
+
+    const handleOpen = () => {
+        setVisible(true);
+    };
+
+    const handleClose = () => {
+        setVisible(false);
+    };
+    
+    
+
+    return (
+        <div>
+
+
+            <div className='flex ml-9 py-2  items-center'>
+                {/* add user */}
+                <button className='bg-blue-600 hover:bg-blue-800 border rounded ml-4  py-2 px-2 text-white '
+                    onClick={handleOpen}>
+                    Add User
+                </button>
+                {/* search */}
+
+                <div className="inputSearch ml-12">
+                    <label htmlFor="searchInput" className="searchInput">
+                        <i class="fa-sharp fa-solid fa-magnifying-glass"></i>
+                        <input
+                            className="formSearch"
+                            type="search"
+                            id="searchInput"
+                            value={searchKeyword}
+                            onChange={handleSearchChange}
+                        />
+                    </label>
+                </div>
+            </div>
+            <br />
+            {/* popup */}
+            <Modal
+                visible={visible}
+                centered
+                onCancel={handleClose}
+                footer={null}
+                
+            >
+                <AddUser/>
+                
+            </Modal>
+
+            <Table
+                columns={columns}
+                dataSource={data}
+                onChange={onChange}
+                showSorterTooltip={{
+                    target: 'sorter-icon',
+                }}
+                className='mx-12'
+            />
+
+            <div className='popup_drawer'>
+                <Drawer
+                    title="Edit Project"
+                    placement="right"
+                    width={500}
+                    onClose={onCloseDrawer}
+                    open={open}
+                >
+                    <form className="space-y-4 md:space-y-6 text-black" action="#" onSubmit={handleSubmit}>
+                        <InputCustom touched={touched.id} onBlur={handleBlur} value={values.id}
+                            className='mt-0 mb-2 ' name={"id"} label={"User ID"} placeholder={"Type Project ID"} />
+                        {errors.id && touched.id ? (<p className='text-sm text-red-500' >{errors.id} </p>) : null}
+
+                        <InputCustom touched={touched.email} onBlur={handleBlur} value={values.email} onChange={
+                            (e) => {
+                                handleChange(e);
+                            }}
+                            className='mt-0 mb-2 bg-white ' name={"email"} label={"Email"} placeholder={"Type Email"} />
+                        {errors.email && touched.email ? (<p className='text-sm text-red-500' >{errors.email} </p>) : null}
+
+                        <InputCustom touched={touched.name} onBlur={handleBlur} value={values.name} onChange={
+                            (e) => {
+                                handleChange(e);
+                            }}
+                            className='mt-0 mb-2 bg-white ' name={"name"} label={"Name"} placeholder={"Type Name"} />
+                        {errors.name && touched.name ? (<p className='text-sm text-red-500' >{errors.name} </p>) : null}
+
+                        <InputCustom touched={touched.phoneNumber} onBlur={handleBlur} value={values.phoneNumber} onChange={
+                            (e) => {
+                                handleChange(e);
+                            }}
+                            className='mt-0 mb-2 bg-white ' name={"phoneNumber"} label={"Phone Number"} placeholder={"Type Phone Number"} />
+                        {errors.phoneNumber && touched.phoneNumber ? (<p className='text-sm text-red-500' >{errors.phoneNumber} </p>) : null}
+
+
+
+
+
+
+                        {/* foot popup */}
+                        <div className='flex align-items-center justify-between mt-5 '>
+                            <div className='space-x-1'>
+                                <button type='submit' className=' py-2 px-5 rounded bg-blue-600 text-white hover:bg-blue-700' >Update User</button>
+                            </div>
+                        </div>
+
+                    </form>
+
+
+
+                </Drawer>
+            </div>
+
+        </div>
+    )
+}
+
+export default UserManagerment
---
+++ src/layout/AddUser/AddUser.jsx
@@ -0,0 +1,143 @@
+import React, { useContext } from 'react'
+import * as yup from 'yup'
+import { useFormik } from 'formik';
+import { NavLink, useNavigate } from 'react-router-dom';
+// import { NotifyContext } from '../../template/Template';
+import InputCustom from '../../Components/Input/InputCustom';
+import { quanLyTaiKhoanServ } from '../../services/quanLyTaiKhoan/quanLyTaiKhoanServ';
+
+
+const AddUser = ({setIsReload}) => {
+    //   let notify=useContext(NotifyContext)
+    
+    
+
+    let { handleChange, handleSubmit, values, errors, handleBlur, touched } = useFormik({
+        initialValues: {
+            email: ""
+            , passWord: ""
+            , name: ""
+            , phoneNumber: ""
+        },
+        onSubmit: async (values) => {
+            //   quanLyNguoiDungServ
+            //   .dangky(values)
+            //   .then((res) => {
+            //     // notify("Đăng ký thành công, khách hàng sẽ được chuyển hướng về trang đăng nhập")
+            //     setTimeout(() => {navigate("/login")  },2000)
+            //   })
+            //   .catch((error) => {
+            //     // notify(error.response.data.message);
+            //   })
+
+            quanLyTaiKhoanServ.dangKi(values)
+                .then((res) => {
+                    console.log(res.data.message);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+
+
+
+
+        },
+        validationSchema: yup.object({
+            name: yup.string().required("Vui lòng không bỏ trống")
+            , passWord: yup.string().matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{6,}$/, "Mật khẩu có ít nhất 6 ký tự và Bao gồm: chữ hoa, chữ thường, số").required("Vui lòng không bỏ trống")
+            , email: yup.string().email("Vui lòng kiểm tra định dạng email").required("Vui lòng không bỏ trống")
+            , phoneNumber: yup.string().matches(/^(0[2|3|4|5|6|7|8|9]{1})([0-9]{8,9})$/, "Vui lòng nhập đúng số điện thoại").required("Vui lòng không bỏ trống")
+        })
+
+
+    })
+
+    return (
+        <section className="bg-gray-50 w-full py-5 rounded ">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto  lg:py-0">
+
+                <div className="w-full bg-white rounded-lg shadow  md:mt-0 xl:p-0 ">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                            Create User
+                        </h1>
+                        <form className="space-y-4 md:space-y-6 text-black" action="#" onSubmit={handleSubmit}>
+                            <div >
+                                <InputCustom
+                                    placeholder="Vui lòng nhập email"
+                                    id="email"
+                                    label="Email"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    error={errors.email}
+                                    touched={touched.email}
+                                    name="email"
+                                    value={values.email}
+                                />
+                            </div>
+
+                            <div >
+                                <InputCustom
+                                    placeholder="Vui lòng nhập mật khẩu"
+                                    id="passWord"
+                                    label="Mật Khẩu"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    error={errors.passWord}
+                                    touched={touched.passWord}
+                                    name="passWord"
+                                    value={values.passWord}
+                                // type='password'
+                                />
+                            </div>
+
+
+
+                            <div >
+                                <InputCustom
+                                    placeholder="Vui lòng nhập Tên"
+                                    id="name"
+                                    label="Họ Tên"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    error={errors.name}
+                                    touched={touched.name}
+                                    name="name"
+                                    value={values.name}
+                                />
+                            </div>
+
+                            <div >
+                                <InputCustom
+                                    placeholder="Vui lòng nhập số điện thoại"
+                                    id="phoneNumber"
+                                    label="Số điện thoại"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    error={errors.phoneNumber}
+                                    touched={touched.phoneNumber}
+                                    name="phoneNumber"
+                                    value={values.phoneNumber}
+                                />
+                            </div>
+
+
+                            {/* button */}
+                            <div>
+                                <button type="submit" className="w-full text-white bg-black border-black  hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Create</button>
+                            </div>
+                            {/* <div>
+                                <button 
+                                onClick={handelNhap}>nhap</button>
+                            </div> */}
+
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default AddUser
